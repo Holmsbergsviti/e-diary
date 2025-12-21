35bb528122f9054644f3c61c2ad4214dfc8b74ef
Bill-RS <250559790+Bill-RS@users.noreply.github.com>
2025-12-21 19:42:32 +0100

    Add files via upload
    (This file defines a class in python call 'student')

---
+++ backend/Student.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file which is done by Bill
+"""
+
+class Students:
+    def _info_(self,ID,name,surname,gender,grade,class1,subjects,comment):
+        self.name=name
+        self._ID=ID
+        self.surname=name
+        self.gender=gender
+        self.grade=grade
+        self.c=class1
+        self.sub=subjects
+        self.comment=comment
+    def GetID(self):
+        return self._ID
+    
