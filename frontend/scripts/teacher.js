82756b92c81b870fde9b960a5d5bf6b16f7f98c5
Bill-RS <250559790+Bill-RS@users.noreply.github.com>
2026-03-06 14:12:56 +0100

    show/hide button

--- frontend/scripts/teacher.js
+++ frontend/scripts/teacher.js
@@ -852,7 +852,7 @@ async function saveBehavioral() {
 }
 
 function showSettings() {
-    checkbox = document.getElementById("showSettings")
+    checkbox = document.getElementById("settingsBtn")
     if (checkbox.checked) {
         checkbox.hidden = false;
     } else {
