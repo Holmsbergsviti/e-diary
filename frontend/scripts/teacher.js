06061cc9b4b16fcc288a2436d5440bdbc4c8953f
Bill-RS <250559790+Bill-RS@users.noreply.github.com>
2026-03-06 14:54:31 +0100

    show/hide button

--- frontend/scripts/teacher.js
+++ frontend/scripts/teacher.js
@@ -854,11 +854,15 @@ async function saveBehavioral() {
 function showSettings() {
         // Get all elements with the class "group-checkbox"
     const checkboxes = document.querySelectorAll('.group-checkbox');
-
     // Iterate over the collection and set the display style
     checkboxes.forEach(checkbox => {
-        checkbox.style.display = 'none';
-        // You might also want to hide the associated labels if they exist separately
+        if (checkbox.style.display='none'){
+            checkbox.style.display = 'block'
+        }
+        else {
+            checkbox.style.display='none'
+        }
+        
     });
 }
 
